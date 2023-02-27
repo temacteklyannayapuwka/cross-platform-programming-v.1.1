d422ee40333e6cf03edea06c872f17f31997db9b
Artem Bakanov <rtmbakanov@gmail.com>
2023-02-27 16:05:13 +0300

    added condition

--- app2.cpp
+++ app2.cpp
@@ -17,3 +17,11 @@ int main()
 	printf("Введите число x\n");
 	scanf_s("%d", &x);
 
+if (a < 0 && c != 0)
+		f = (a * x ^ 2) + (b * x) + c;
+	else
+		if (a > 0 && c == 0, x != c)
+			f = -a / x - c;
+		else
+			f = x / c;
+
