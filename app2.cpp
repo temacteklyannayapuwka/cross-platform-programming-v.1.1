5d2ef5a45945b819fb37f667b406cf9b7aa11981
Artem Bakanov <rtmbakanov@gmail.com>
2023-02-27 16:02:31 +0300

    added data input

--- app2.cpp
+++ app2.cpp
@@ -6,3 +6,14 @@
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	
+	int a, b, c, x, f;
+	printf("Введите число a\n");
+	scanf_s("%d", &a);
+	printf("Введите число b\n");
+	scanf_s("%d", &b);
+	printf("Введите число c\n");
+	scanf_s("%d", &c);
+	printf("Введите число x\n");
+	scanf_s("%d", &x);
+
